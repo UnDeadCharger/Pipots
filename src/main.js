872538967074dae181d6b3c17c2b9140cf0bdf13
Truong Gia Huy <huy.truong@devsamurai.com>
2026-01-03 14:42:19 +0700

    ppi feat: paddles and working keybind

--- src/main.js
+++ src/main.js
@@ -1,30 +1,69 @@
 class App {
   constructor() {
     this.root = document.documentElement;
+
+    //Board
     this.board = document.querySelector(".board");
     this.boardCoord = this.board.getBoundingClientRect();
-    this.startPosX = this.boardCoord.x / 2;
-    this.startPosY = this.boardCoord.y / 2;
+    this.startPosX = this.boardCoord.width / 2;
+    this.startPosY = this.boardCoord.height / 2;
 
+    //Ball
     this.ball = document.querySelector(".ball");
-    this.ballX = 0;
-    this.ballY = 0;
-    this.ballVX = 50;
-    this.ballVY = 50;
-    this.ballArea = this.ball.getBoundingClientRect().width;
+    this.grid = this.ball.getBoundingClientRect().width;
+    this.ballX = this.startPosX;
+    this.ballY = this.startPosY;
+    this.ballVX = 10;
+    this.ballVY = 10;
+    this.ballArea = this.grid;
+
+    //Paddle:
+    this.paddleL = document.querySelector(".paddle-left");
+    this.paddleR = document.querySelector(".paddle-right");
+
+    this.paddleLY = this.paddleL.getBoundingClientRect().y;
+    this.paddleRY = this.paddleR.getBoundingClientRect().y;
+    this.paddleLVY = 0;
+    this.paddleRVY = 0;
+    // this.paddle = document.querySelector(".paddle-right");d
+    //disable input
+    this.paddleLDisabled = false;
+    this.paddleRDisabled = false;
 
     //Why do this?
     this.loop = this.loop.bind(this);
+    this.setupEvents = this.setupEvents.bind(this);
     this.isAnimating = false;
   }
 
+  //Collision detection
+  getBall() {
+    return {
+      x: this.ballX,
+      y: this.ballY,
+      width: this.ballArea,
+      height: this.ballArea,
+    };
+  }
+
+  //   getPaddleLeft() {
+  //     const paddle = this.paddle.getBoundingClientRect();
+  //     return paddle;
+  //   }
+
+  collides(rect1, rect2) {
+    return !(
+      rect1.x > rect2.x + rect2.width ||
+      rect1.x + rect1.width < rect2.x ||
+      rect1.y > rect2.y + rect2.height ||
+      rect1.y + rect1.height < rect2.y
+    );
+  }
+
   impactFrame(times) {
-    console.log("IMPACT");
     let counter = 0;
     // this.root.style.background = "#2a2a2a";
     const intervalId = setInterval(() => {
-      console.log("impact", counter);
-
       if (counter >= times * 2) {
         this.root.style.background = "black";
         clearInterval(intervalId);
@@ -48,11 +87,9 @@ class App {
   checkHighspeed() {
     if (Math.abs(this.ballVY) > 100 || Math.abs(this.ballVX) > 100) {
       const times = Math.abs(this.ballVY) / 50 - 1;
-      console.log("pretrig impact");
       this.impactFrame(times);
       this.isAnimating = true;
       setTimeout(() => {
-        console.log("trig impact");
         this.isAnimating = false;
       }, 10 * Math.abs(this.ballVY));
     }
@@ -60,6 +97,40 @@ class App {
 
   async loop() {
     requestAnimationFrame(this.loop);
+
+    // move paddles by their velocity
+    this.paddleLY += this.paddleLVY;
+    this.paddleRY += this.paddleRVY;
+    // prevent paddles from going through walls
+    if (this.paddleLY < this.grid) {
+      this.paddleLY = this.grid;
+    } else if (
+      this.paddleLY >
+      this.boardCoord.height -
+        this.paddleL.getBoundingClientRect().height -
+        this.grid
+    ) {
+      this.paddleLY =
+        this.boardCoord.height -
+        this.paddleL.getBoundingClientRect().height -
+        this.grid;
+    }
+
+    if (this.paddleRY < this.grid) {
+      this.paddleRY = this.grid;
+    } else if (
+      this.paddleRY >
+      this.boardCoord.height -
+        this.paddleR.getBoundingClientRect().height -
+        this.grid
+    ) {
+      this.paddleRY =
+        this.boardCoord.height -
+        this.paddleR.getBoundingClientRect().height -
+        this.grid;
+    }
+    this.paddleL.style.transform = `translateY(${this.paddleLY}px)`;
+    this.paddleR.style.transform = `translateY(${this.paddleRY}px)`;
 
     if (this.isAnimating) {
       //Play flashing animation
@@ -74,43 +145,48 @@ class App {
     // move ball by its velocity
     this.ballX += this.ballVX;
     this.ballY += this.ballVY;
+    // check collision
+    if (this.collides(this.getBall(), this.paddleL.getBoundingClientRect())) {
+      this.ballX =
+        this.paddleL.getBoundingClientRect().x +
+        this.paddleL.getBoundingClientRect().width;
+      this.ballVX *= -1;
+      this.accelerate();
+      this.checkHighspeed();
+    }
+    if (this.collides(this.getBall(), this.paddleR.getBoundingClientRect())) {
+      this.ballX =
+        this.paddleR.getBoundingClientRect().x -
+        this.paddleR.getBoundingClientRect().width;
+      this.ballVX *= -1;
+      this.accelerate();
+      this.checkHighspeed();
+    }
 
     //prevent ball from going through walls by changing its velocity
     if (this.ballY < this.ballArea) {
       //when ball touch top wall
       this.ballY = this.ballArea;
       this.ballVY *= -1;
-      this.accelerate();
     } else if (
       this.ballY + this.ballArea >
       this.boardCoord.height - this.ballArea
     ) {
       this.ballY = this.boardCoord.height - this.ballArea * 2;
       this.ballVY *= -1;
-      this.accelerate();
     }
 
     if (this.ballX < this.ballArea) {
       //when ball touch left wall
       this.ballX = this.ballArea;
       this.ballVX *= -1;
-      this.accelerate();
-      this.checkHighspeed();
     } else if (
       this.ballX + this.ballArea >
       this.boardCoord.width - this.ballArea
     ) {
       this.ballX = this.boardCoord.width - this.ballArea * 2;
       this.ballVX *= -1;
-      this.accelerate();
-      this.checkHighspeed();
     }
-    console.log(
-      this.boardCoord.width - this.ballArea,
-      this.ballX,
-      this.boardCoord.height - this.ballArea,
-      this.ballY
-    );
     //update ball position
     this.ball.style.transform = `translate(${this.ballX}px, ${this.ballY}px)`;
   }
@@ -125,7 +201,42 @@ class App {
     this.velocityY = 5;
   }
 
-  setupEvents() {}
+  setupEvents() {
+    const paddleSpeed = 10;
+    // listen to keyboard events to move the paddles
+    document.addEventListener("keydown", (e) => {
+      // up arrow key
+      if (e.key === "ArrowUp") {
+        this.paddleRVY = -paddleSpeed;
+      }
+      // down arrow key
+      else if (e.key === "ArrowDown") {
+        this.paddleRVY = paddleSpeed;
+      }
+
+      // w key
+      if (e.key === "w") {
+        this.paddleLVY = -paddleSpeed;
+      }
+      // s key
+      else if (e.key === "s") {
+        this.paddleLVY = paddleSpeed;
+      }
+    });
+
+    // listen to keyboard events to stop the paddle if key is released
+    document.addEventListener("keyup", (e) => {
+      if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+        this.paddleRVY = 0;
+      }
+
+      if (e.key === "w" || e.key === "s") {
+        this.paddleLVY = 0;
+      }
+    });
+
+    this.ball.style.transform = `translate(${this.startPosX}px, ${this.startPosY}px)`;
+  }
 }
 
 const app = new App();
